e160b0637bc02190e3a27f1c03275d8c35a6e01f
coleoptere69 <coleoptere151102@gmail.com>
2024-12-01 20:10:22 +0300

    part 1 logic

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"slices"
+)
 
 func main() {
-	fmt.Println("Zdarova zaebal")
+	left := []int{3, 4, 2, 1, 3, 3}
+	right := []int{4, 3, 5, 3, 9, 3}
+	slices.Sort(left)
+	slices.Sort(right)
+	sum := 0
+	for i := range left {
+		sum += int(math.Abs(float64(left[i] - right[i])))
+	}
+	fmt.Println("part 1:", sum)
 }
