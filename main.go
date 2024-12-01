872ade073047fceec47b91753cb2a0e4aff08b48
coleoptere69 <coleoptere151102@gmail.com>
2024-12-01 21:01:57 +0300

    part 2 complete

--- main.go
+++ main.go
@@ -9,13 +9,41 @@ import (
 )
 
 func main() {
+	part1()
+	part2()
+}
+
+func part1() {
+	left, right := ParseInput()
+	slices.Sort(left)
+	slices.Sort(right)
+	sum := 0
+	for i := range left {
+		sum += int(math.Abs(float64(left[i] - right[i])))
+	}
+	fmt.Println("part 1:", sum)
+}
+
+func part2() {
+	left, right := ParseInput()
+
+	freq := map[int]int{}
+	for _, num := range right {
+		freq[num]++
+	}
+	sum := 0
+	for _, num := range left {
+		sum += num * freq[num]
+	}
+	fmt.Println("part 2:", sum)
+}
+
+func ParseInput() (left, right []int) {
 	file, err := os.Open("input1.txt")
 	if err != nil {
 		panic(err)
 	}
 	scanner := bufio.NewScanner(file)
-	left := []int{}
-	right := []int{}
 	for scanner.Scan() {
 		leftNum, rightNum := 0, 0
 		_, err = fmt.Sscanf(scanner.Text(), "%d   %d", &leftNum, &rightNum)
@@ -25,12 +53,5 @@ func main() {
 		left = append(left, leftNum)
 		right = append(right, rightNum)
 	}
-
-	slices.Sort(left)
-	slices.Sort(right)
-	sum := 0
-	for i := range left {
-		sum += int(math.Abs(float64(left[i] - right[i])))
-	}
-	fmt.Println("part 1:", sum)
+	return left, right
 }
